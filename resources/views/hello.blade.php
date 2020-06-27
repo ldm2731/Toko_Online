48a51bd67cd31299ceaf8f62be8bae7a63b14382
Imin <dimas.ma13@gmail.com>
2020-06-27 17:13:16 +0700

    rename file blade

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    TOKLEN
+                    APIT
                 </div>
 
                 <div class="links">
