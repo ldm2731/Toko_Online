e3b7984b99b4fe1967505dbf31d06f8db5027536
Imin <dimas.ma13@gmail.com>
2020-06-27 16:59:16 +0700

    ganti judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TOKLEN
                 </div>
 
                 <div class="links">
