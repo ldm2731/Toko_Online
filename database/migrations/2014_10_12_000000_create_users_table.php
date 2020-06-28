7b6797dfde67a603a6b59bbbdf82e8a54df89147
ldm2731 <apetmichael@gmail.com>
2020-06-28 13:00:17 +0700

    table_user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
+            $table->string('password');
+            $table->string('role_id');
+            $table->string('alamat');
+            $table->string('no_tlpn');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
