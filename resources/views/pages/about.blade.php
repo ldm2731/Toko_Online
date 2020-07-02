acdd390e42d4576ed9562fc9c377af2731f078da
ahmadmuhammad <ahmad.mh508@gmail.com>
2020-07-02 19:54:47 +0700

    deskripsi

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,7 +6,11 @@
     <div class="row h-100 align-items-center py-5">
       <div class="col-lg-6">
         <h1 class="display-4">About AM DESAIN</h1>
-        <p class="lead text-muted mb-0">Create a minimal about us page using Bootstrap 4.</p> 
+        <p class="lead text-muted mb-0">Hi, we are AM Design, is a place for you. You can give your creativity to be more products, 
+        AM Design was established in 2015, Our focus is in the field of Convection and Souvenir, 
+        we make everything from T-shirts, Jackets, Shirts, Jersey, Polo Almamater etc. . 
+        and also what we make are Rubber Souvenirs, Rubber Keychains, Rubber Labels etc.
+        </p> 
       </div>
       <div class="col-lg-6 d-none d-lg-block"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834136/illus_kftyh4.png" alt="" class="img-fluid"></div>
     </div>
@@ -18,7 +22,7 @@
     <div class="row align-items-center mb-5">
       <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
         <h2 class="font-weight-light">VISI</h2>
-        <p class="font-italic text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+        <p class="font-italic text-muted mb-4">simplify your printing needs.</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
       </div>
       <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834139/img-1_e25nvh.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
     </div>
@@ -26,7 +30,9 @@
       <div class="col-lg-5 px-5 mx-auto"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834136/img-2_vdgqgn.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
       <div class="col-lg-6"><i class="fa fa-leaf fa-2x mb-3 text-primary"></i>
         <h2 class="font-weight-light">MISI</h2>
-        <p class="font-italic text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+        <p class="font-italic text-muted mb-4">
+        doing all the promotional needs, souvenirs
+        </p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
       </div>
     </div>
   </div>
