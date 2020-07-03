a5b8f532d87c2770bbf9aa6654e1939fd666e6a0
LaDavid <apetmichael@gmail.com>
2020-07-03 23:41:59 +0700

    Migrate About

---
+++ database/migrations/2020_07_03_161458_create_abouts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAboutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->text('deskripsi');
+            $table->string('gambar');
+            $table->text('visi');
+            $table->string('gambar_visi');
+            $table->text('misi');
+            $table->string('gambar_misi');
+            $table->text('deskripsi_team');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abouts');
+    }
+}
