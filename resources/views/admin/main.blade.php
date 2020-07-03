0bef71fe3452faca33292414f4dde0373dc35884
ahmadmuhammad <ahmad.mh508@gmail.com>
2020-07-04 03:26:01 +0700

    form user

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -82,7 +82,7 @@
       </div>
     </nav>
 
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4 pt-4">
         @yield('content')
     </main>
   </div>
--- resources/views/admin/pages/user.blade.php
+++ resources/views/admin/pages/user.blade.php
@@ -3,8 +3,53 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
-        ki halaman user
+        <div class="card">
+            <div class="card-body">
+                <form method="">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="name">Nama</label>
+                          <input type="text" name="name" class="form-control" placeholder="Your Name *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                          <input type="text" name="email" class="form-control" placeholder="Your Email *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                          <input type="text" name="password" class="form-control" placeholder="Password *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <label for="alamat">alamat</label>
+                          <input type="text" name="alamat" class="form-control" placeholder="Alamat *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <label for="no_tlpn">No Telpon</label>
+                          <input type="text" name="no_telpn" class="form-control" placeholder="No Telpon *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <label for="privillage">Privillage</label>
+                            <select class="custom-select">
+                                <option selected>privillage</option>
+                                <option value="1">Admin</option>
+                                <option value="2">User</option>
+                            </select>
+        
+                        </div>
+                        
+                            
+                        <div class="form-group">
+                          <input onclick="myFunction()" type="submit" name="btnSubmit" class="btnContact" value="Send Message" />
+                        </div>
+                      </div>
+                    </div>
+                </form>
+            </div>
+          </div>
+        
     </div>
 </div>
+
     
 @endsection
